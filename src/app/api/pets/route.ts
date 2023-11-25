d484b3ae1cdd40edcc3797b7f001e6b560f5abb1
Fireflies56 <shahriariqbal56@iut-dhaka.edu>
2023-11-25 15:29:23 +0900

    update_25/11/2023

--- src/app/api/pets/route.ts
+++ src/app/api/pets/route.ts
@@ -1,15 +1,21 @@
 import { NextResponse } from 'next/server'
 
 import prisma from '../../../../lib/prisma'
+import { type NextRequest } from 'next/server'
 
 // GET /api/pets
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const queryName = searchParams.get('name')
+  // findMany returns a list of records.
   const pets = await prisma.pet.findMany({
     // sort by id ascending
     orderBy: { id: 'asc' },
-    // include owner data
+    // select only id, name, imageUrl, and owner.name
     select: { id: true, name: true, imageUrl: true, owner: { select: { name: true } } },
+    where: queryName ? { name: { contains: queryName } } : undefined,
   })
+  // return Response with pets to json
   return NextResponse.json({ pets })
 }
 
